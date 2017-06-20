2a031696ced3087a0a5dc6b583cb8afafbae87b1
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-20 20:28:58 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    count_odd = 0
+    count_even = 0
+    d = {}
+    for x in list_of_nums:
+            if not x % 2:
+        	     count_even+=1
+            else:
+        	     count_odd+=1
+    d = { "ODD" : count_odd , "EVEN" : count_even}
+    return d
+
+my_list = [1, 2, 3, 5, 8, 9]
+solution(my_list)
